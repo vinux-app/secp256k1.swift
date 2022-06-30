5fee95663dd5a312b360d3727c7bee55fa8c15c8
William Casarin <jb55@jb55.com>
2022-06-30 10:01:11 -0700

    context: make public
    Signed-off-by: William Casarin <jb55@jb55.com>

--- Sources/implementation/secp256k1.swift
+++ Sources/implementation/secp256k1.swift
@@ -35,7 +35,7 @@ public extension secp256k1 {
             return context
         }
 
-        static let raw = try! secp256k1.Context.create()
+        public static let raw = try! secp256k1.Context.create()
     }
 }
 
